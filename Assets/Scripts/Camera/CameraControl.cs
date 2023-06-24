5c6670335257636be118b5310278122110b0e027
Miata <liuyuran719829142@hotmail.com>
2023-06-24 18:06:48 +0800

    add input system support

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,17 +10,29 @@ namespace Camera {
         private float _yaw;
         private float _pitch;
 
+        private KeyActionSettings _keyActionSettings;
+        private KeyActionSettings.StandardActions _standardActions;
 
         private void Start() {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false; // 隐藏鼠标
             var eulerAngles = transform.eulerAngles;
             _yaw = eulerAngles.y;
             _pitch = eulerAngles.x;
         }
 
+        private void OnEnable() {
+            _keyActionSettings = new KeyActionSettings();
+            _keyActionSettings.Enable();
+            _standardActions = _keyActionSettings.standard;
+        }
+
+        private void OnDisable() {
+            _keyActionSettings.Dispose();
+        }
+
         private void Update() {
-            var gamepad = InputSystem.GetDevice<Gamepad>();
-            if (gamepad == null) return;
-            var rightStick = gamepad.rightStick.ReadValue();
+            var rightStick = _standardActions.Look.ReadValue<Vector2>();
             _yaw += lookSpeedH * rightStick.x;
             _pitch -= lookSpeedV * rightStick.y;
             transform.eulerAngles = new Vector3(_pitch, _yaw, 0f);
--- Assets/Scripts/Camera/PlayerMovementSystem.cs
+++ Assets/Scripts/Camera/PlayerMovementSystem.cs
@@ -1,12 +1,26 @@
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Camera {
+    [BurstCompile]
     public partial class PlayerMovementSystem : SystemBase {
+        private KeyActionSettings _keyActionSettings;
+        private KeyActionSettings.StandardActions _standardActions;
+        protected override void OnCreate() {
+            _keyActionSettings = new KeyActionSettings();
+            _keyActionSettings.Enable();
+            _standardActions = _keyActionSettings.standard;
+        }
+
+        protected override void OnDestroy() {
+            _keyActionSettings.Dispose();
+        }
+
+        [BurstCompile]
         protected override void OnUpdate() {
             Entities.WithAll<Player>()
                 .ForEach((ref PhysicsMass mass) => {
@@ -20,36 +34,35 @@ namespace Camera {
             var playerPos = new float2(0, 0);
             var cameraRotation = cameraTransform.rotation.eulerAngles;
             var currentQuaternion = Quaternion.Euler(0, cameraRotation.y, 0);
-            var gamepad = InputSystem.GetDevice<Gamepad>();
-            if (gamepad == null) return;
-            var leftStick = gamepad.leftStick.ReadValue();
-            Entities.WithAll<Player>()
-                .ForEach(
-                    (Entity entity, ref LocalToWorld localToWorld, ref PhysicsVelocity vel, ref PhysicsMass mass) => {
-                        mass.InverseInertia.x = 0;
-                        mass.InverseInertia.z = 0;
-                        cameraPos = localToWorld.Position + new float3(0, 1.5f, 0);
-                        entityManager.SetComponentData(entity, new LocalToWorld {
-                            Value = float4x4.TRS(
-                                localToWorld.Position,
-                                currentQuaternion,
-                                new float3(1, 1, 1)
-                            )
-                        });
-                        if (leftStick.y != 0) {
-                            playerPos = localToWorld.Forward.xz * leftStick.y * speed;
-                        }
+            var leftStick = _standardActions.Move.ReadValue<Vector2>();
+            var jump = _standardActions.Jump.triggered;
+            Entities.WithAll<Player>().ForEach(
+                (Entity entity, ref LocalToWorld localToWorld, ref PhysicsVelocity vel, ref PhysicsMass mass) => {
+                    mass.InverseInertia.x = 0;
+                    mass.InverseInertia.z = 0;
+                    cameraPos = localToWorld.Position + new float3(0, 1.5f, 0);
+                    entityManager.SetComponentData(entity, new LocalToWorld {
+                        Value = float4x4.TRS(
+                            localToWorld.Position,
+                            currentQuaternion,
+                            new float3(1, 1, 1)
+                        )
+                    });
+                    if (leftStick.y != 0) {
+                        playerPos = localToWorld.Forward.xz * leftStick.y * speed;
+                    }
 
-                        if (leftStick.x != 0) {
-                            playerPos += localToWorld.Right.xz * leftStick.x * speed;
-                        }
+                    if (leftStick.x != 0) {
+                        playerPos += localToWorld.Right.xz * leftStick.x * speed;
+                    }
 
-                        if (gamepad.yButton.isPressed) {
-                            vel.Linear.y = localToWorld.Up.y * speed;
-                        }
+                    if (jump) {
+                        vel.Linear.y = localToWorld.Up.y * 2 * speed;
+                    }
 
-                        vel.Linear.xz = playerPos;
-                    }).WithoutBurst().Run();
+                    vel.Linear.xz = playerPos;
+                }
+            ).WithoutBurst().Run();
             cameraTransform.position = cameraPos;
         }
     }
